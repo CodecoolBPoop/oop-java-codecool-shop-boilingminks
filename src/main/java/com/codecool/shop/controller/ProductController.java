81705925e17a84b2a18eda95d35c4ceccecf8c79
gaborkallos <gaborkallos@gmail.com>
2019-01-16 12:50:39 +0100

    shoppingcart.html in progress

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -43,7 +43,6 @@ public class ProductController extends HttpServlet {
         context.setVariable("sum_of_items", shoppingCarts.getItemsInCarts().get(userId));
         context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         context.setVariable("categories", productCategoryDataStore.getAll());
         context.setVariable("products", productDataStore.getAll());
 
--- src/main/java/com/codecool/shop/controller/ShoppingCartHTML.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartHTML.java
@@ -3,8 +3,11 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -14,23 +17,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/shoppingcart"})
 public class ShoppingCartHTML extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
-        System.err.println("HELLO");
-        ProductDao products = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategories = ProductCategoryDaoMem.getInstance();
+        ShoppingCartDao shoppingCartDao = ShoppingCartDaoMem.getInstance();
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+
+        Map <Product, Integer> cartData = new HashMap<>();
+
+        int userId = 1;
+        Map<Integer, Integer> userCart = shoppingCartDao.getAll().get(userId);
+
+        for (Integer key : userCart.keySet()) {
+            cartData.put(productDataStore.find(key), userCart.get(key) );
+            System.err.println(productDataStore.find(key) + " -- " + userCart.get(key));
+        }
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, resp, request.getServletContext());
 
-        String parameter = request.getParameter("cat");
-        System.out.printf("The request I asked for is %s%n", parameter);
-        context.setVariable("recipient", "World");
-        context.setVariable("categories", productCategories.getAll());
+
+
+       context.setVariable("cart_data", cartData);
 
         engine.process("product/shoppingcart.html", context, resp.getWriter());
 
