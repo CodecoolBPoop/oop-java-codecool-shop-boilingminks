ea78a47896e8cd5fd2eb5d344670998b6fbdd0f2
Evi Kadar <evi.kadar@gmail.com>
2019-01-16 14:15:54 +0100

    Order base structure created with order interface and implementation

---
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -0,0 +1,46 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.model.Order;
+import com.codecool.shop.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDaoMem implements OrderDao {
+    private List<Order> data = new ArrayList<>();
+    private static OrderDaoMem instance = null;
+
+    private OrderDaoMem() {
+    }
+
+    public static OrderDaoMem getInstance() {
+        if (instance == null) {
+            instance = new OrderDaoMem();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public void add(Order order) {
+        order.setId(data.size() + 1);
+        data.add(order);
+
+    }
+
+    @Override
+    public Order find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Order> getBy(User user) {
+        return null;
+    }
+}
---
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -0,0 +1,18 @@
+package com.codecool.shop.model;
+
+
+public class Order {
+    private int id;
+
+    public Order() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
