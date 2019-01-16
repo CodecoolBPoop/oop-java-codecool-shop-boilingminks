995e4fc7c07e07c13a4bc3a2ac83dc8a25880255
Evi Kadar <evi.kadar@gmail.com>
2019-01-16 13:41:35 +0100

    post method for checkout gets data

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -15,6 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.RequestWrapper;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/checkout")
@@ -29,7 +30,35 @@ public class CheckoutController extends HttpServlet {
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
         engine.process("product/checkout.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.printf("We did a post on the checkout page!%n");
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String email = req.getParameter("email");
+        String address = req.getParameter("address");
+        String address2 = req.getParameter("address2");
+        String state = req.getParameter("state");
+        String zip = req.getParameter("zip");
+        String country = req.getParameter("country");
+        String shipping = req.getParameter("shipping");
+        String saveInfo = req.getParameter("saveInfo");
+        System.out.printf("%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n", firstName, lastName, email, address, address2, state, zip, country, shipping, saveInfo);
 
 
     }
+
+
+
+
+
+
+
+
+
+
 }
+
+
