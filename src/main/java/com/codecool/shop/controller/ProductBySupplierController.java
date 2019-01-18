b5ad1f11e512761573c3800b419870b5550a2f58
gaborkallos <gaborkallos@gmail.com>
2019-01-18 08:40:31 +0100

    minor hotfix

--- src/main/java/com/codecool/shop/controller/ProductBySupplierController.java
+++ src/main/java/com/codecool/shop/controller/ProductBySupplierController.java
@@ -3,9 +3,11 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -23,12 +25,18 @@ public class ProductBySupplierController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int supplierId = Integer.parseInt(req.getParameter("id"));
+        int userId = 1; // TODO: USER SYSTEM
+
+
+        ShoppingCartDao shoppingCarts = ShoppingCartDaoMem.getInstance();
         ProductDao productDataStore = ProductDaoMem.getInstance();
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        context.setVariable("sum_of_items", shoppingCarts.getSumOfItems().get(userId));
         context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.find(1));
         context.setVariable("products", productDataStore.getBy(supplierDataStore.find(supplierId)));
--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,11 +1,18 @@
 function animation() {
+
+    function generateRandomInteger(min, max) {
+        return Math.floor(min + Math.random()*(max + 1 - min))
+    }
+
     var elem = document.getElementById("animate");
-    var pos = 200;
-    var id = setInterval(frame, 10);
+    var pos = generateRandomInteger(0, 400);
+    var id = setInterval(frame, 5);
+
+
 
     function frame() {
         if (pos == 1400) {
-            pos = 200;
+            pos = generateRandomInteger(0, 800);
         } else {
             pos++;
             elem.style.left = pos + "px";
