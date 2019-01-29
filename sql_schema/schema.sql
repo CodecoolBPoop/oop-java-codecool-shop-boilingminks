8184bf56de7013f2342625a8ae1514d44e19ee18
gaborkallos <gaborkallos@gmail.com>
2019-01-29 11:21:11 +0100

    hotfix on schema.sql

--- sql_schema/schema.sql
+++ sql_schema/schema.sql
@@ -19,34 +19,36 @@ product (id SERIAL PRIMARY KEY NOT NULL,
          currency VARCHAR NOT NULL,
          supplier_id int NOT NULL,
          category_id int NOT NULL,
-         image_name VARCHAR NOT NULL)
+         image_name VARCHAR NOT NULL);
 
 CREATE TABLE IF NOT EXISTS
 product_category (id SERIAL PRIMARY KEY NOT NULL,
          name VARCHAR NOT NULL,
          description VARCHAR NOT NULL,
-         department VARCHAR NOT NULL)
+         department VARCHAR NOT NULL);
 
 CREATE TABLE IF NOT EXISTS
 supplier (id SERIAL PRIMARY KEY NOT NULL,
          name VARCHAR NOT NULL,
-         description VARCHAR NOT NULL)
+         description VARCHAR NOT NULL);
 
 CREATE TABLE IF NOT EXISTS
 shopping_cart (order_id int NOT NULL,
          product_id int NOT NULL,
-         quantity int NOT NULL)
+         quantity int NOT NULL);
 
 CREATE TABLE IF NOT EXISTS
-orders (order_id id SERIAL PRIMARY KEY NOT NULL,
+orders (id SERIAL PRIMARY KEY NOT NULL,
         user_id int NOT NULL,
-        validated BOOLEAN NOT NULL
-	payment_method VARCHAR NOT NULL)
+        validated BOOLEAN NOT NULL,
+        payment_method VARCHAR NOT NULL);
 
 ALTER TABLE ONLY product
 ADD CONSTRAINT fk_product_category
-FOREIGN KEY (categorty_id)
+FOREIGN KEY (category_id)
 REFERENCES product_category(id);
+
+ALTER TABLE ONLY product
 ADD CONSTRAINT fk_supplier
 FOREIGN KEY (supplier_id)
 REFERENCES supplier(id);
@@ -55,6 +57,8 @@ ALTER TABLE ONLY shopping_cart
 ADD CONSTRAINT fk_product_id
 FOREIGN KEY (product_id)
 REFERENCES product(id);
+
+ALTER TABLE ONLY shopping_cart
 ADD CONSTRAINT fk_order_id
 FOREIGN KEY (order_id)
 REFERENCES orders(id);
