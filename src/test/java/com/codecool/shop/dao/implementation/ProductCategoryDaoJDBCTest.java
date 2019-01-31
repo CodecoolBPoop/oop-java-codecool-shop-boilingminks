ace76a3e117e07961b610c6e0cf571f4470300fa
zsombor <apathy.zsombor@gmail.com>
2019-01-31 13:06:22 +0100

    Finished test for clearing table in ProductCategoryDaoJDBC

--- src/test/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBCTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBCTest.java
@@ -40,5 +40,28 @@ class ProductCategoryDaoJDBCTest {
     void testIsCanClearTable() {
         ProductCategory duckFood = new ProductCategory(1, "Duck Food", "Animal Food", "the most nutritious duck food on the market");
         ProductCategory catFood = new ProductCategory(2, "Cat Food", "Animal Food", "the most nutritious cat food on the market");
+        testInstance.clear();
+        ArrayList<HashMap<String, String>> dbData = connInst.queryAllFromTable("product_category");
+        assertEquals(0, dbData.size());
+    }
+
+    @Test
+    void testIsCanAddEntireList() {
+        ArrayList<ProductCategory> productCategories = new ArrayList<>();
+        productCategories.add(new ProductCategory(1, "Duck Food", "Animal Food", "the most nutritious duck food on the market"));
+        productCategories.add(new ProductCategory(2, "Cat Food", "Animal Food", "the most nutritious cat food on the market"));
+        productCategories.add(new ProductCategory(2, "Horse Food", "Animal Food", "the most nutritious horse food on the market"));
+        productCategories.add(new ProductCategory(2, "Kangaroo Food", "Animal Food", "the most nutritious kangaroo food on the market"));
+        testInstance.addAll(productCategories);
+        ArrayList<HashMap<String, String>> dbData = connInst.queryAllFromTable("product_category");
+        assertEquals(4, dbData.size());
+        dbData.forEach((dbProductCat) -> {
+            productCategories.forEach((definedProductCat) -> {
+                assertEquals(dbProductCat.get("name"), definedProductCat.getName());
+                assertEquals(dbData.get(0).get("department"), new ProductCategory(1, "Duck Food", "Animal Food", "the most nutritious duck food on the market").getDepartment());
+                assertEquals(dbData.get(0).get("description"), new ProductCategory(1, "Duck Food", "Animal Food", "the most nutritious duck food on the market").getDescription());
+            });
+        });
+
     }
 }
