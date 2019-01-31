2164ebd45fb6273f06eb3fae92ef28bbc0c662d3
zsombor <apathy.zsombor@gmail.com>
2019-01-31 10:29:10 +0100

    created first JDBC test for Supplier

--- src/test/java/com/codecool/shop/dao/implementation/SupplierDaoJDBCTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/SupplierDaoJDBCTest.java
@@ -1,36 +1,48 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.config.JdbcConnectivity;
+import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 class SupplierDaoJDBCTest {
 
-    private static SupplierDaoJDBC testInstance;
-
-    public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-        Field instance = SupplierDaoJDBC.class.getDeclaredField("instance");
-        instance.setAccessible(true);
-        instance.set(null, null);
-        testInstance = new SupplierDaoJDBC();
-    }
+    private static SupplierDaoJDBC testInstance = SupplierDaoJDBC.getInstance();
+    private static JdbcConnectivity connInst = JdbcConnectivity.getInstance();
 
     @BeforeAll
     public static void setUp() {
-
+        JdbcConnectivity.getInstance().executeUpdateFromFile("sql_schema/schema.sql");
     }
 
     @AfterAll
-    public static void tearDown() {
+    public static void tearDown() throws SQLException {
 
     }
 
     @Test
     public void testIsWorking() {
-
+        ArrayList<Supplier> sl = new ArrayList<>();
+        Supplier sASDF = new Supplier(1,"ASDF", "best supplier ASDF");
+        Supplier sQWER = new Supplier(2, "QWER", "best supplier QWER");
+        sl.add(sASDF);
+        sl.add(sQWER);
+        testInstance.loadAll(sl);
+        ArrayList<HashMap<String,String>> d = connInst.queryAllFromTable("supplier");
+        d.forEach((e) -> {
+            sl.forEach((s) -> {
+                if (s.getId() == Integer.parseInt(e.get("id"))) {
+                    assertEquals(s.getName(), e.get("name"));
+                    assertEquals(s.getDescription(), e.get("description"));
+                }
+            });
+        });
     }
 }
