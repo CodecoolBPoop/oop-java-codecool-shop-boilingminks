2a11157b8555dc306ccda81c817b9499e688e607
zsombor <apathy.zsombor@gmail.com>
2019-01-31 11:28:09 +0100

    Added test for add single item in ProductCategoryDaoJDBC

---
+++ src/test/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBCTest.java
@@ -0,0 +1,38 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.config.JdbcConnectivity;
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductCategoryDaoJDBCTest {
+
+    private static ProductCategoryDaoJDBC testInstance = ProductCategoryDaoJDBC.getInstance();
+    private static JdbcConnectivity connInst = JdbcConnectivity.getInstance();
+
+    @BeforeEach
+    void setUp() {
+        JdbcConnectivity.getInstance().executeUpdateFromFile("sql_schema/schema.sql");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testIsCanAddSingleItem() {
+        ProductCategory duckFood = new ProductCategory(1, "Duck Food", "Animal Food", "the most nutritious duck food on the market");
+        testInstance.add(duckFood);
+        ArrayList<HashMap<String, String>> dbData = connInst.queryAllFromTable("product_category");
+        assertEquals(1, dbData.size());
+        assertEquals(dbData.get(0).get("name"), duckFood.getName());
+        assertEquals(dbData.get(0).get("department"), duckFood.getDepartment());
+        assertEquals(dbData.get(0).get("description"), duckFood.getDescription());
+    }
+}
