9bdfc0b727c33152767a6094a268c5ab04bd9d36
zsombor <apathy.zsombor@gmail.com>
2019-01-31 10:52:52 +0100

    fixed warnings

--- src/test/java/com/codecool/shop/dao/implementation/SupplierDaoJDBCTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/SupplierDaoJDBCTest.java
@@ -5,12 +5,11 @@ import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.lang.reflect.*;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SupplierDaoJDBCTest {
 
@@ -18,31 +17,29 @@ class SupplierDaoJDBCTest {
     private static JdbcConnectivity connInst = JdbcConnectivity.getInstance();
 
     @BeforeAll
-    public static void setUp() {
+    static void setUp() {
         JdbcConnectivity.getInstance().executeUpdateFromFile("sql_schema/schema.sql");
     }
 
     @AfterAll
-    public static void tearDown() throws SQLException {
+    static void tearDown() {
 
     }
 
     @Test
-    public void testIsLoadAllMethodIsWorkingCorrectly() {
+    void testIsLoadAllMethodIsWorkingCorrectly() {
         ArrayList<Supplier> sl = new ArrayList<>();
-        Supplier sASDF = new Supplier(1,"ASDF", "best supplier ASDF");
-        Supplier sQWER = new Supplier(2, "QWER", "best supplier QWER");
-        sl.add(sASDF);
-        sl.add(sQWER);
+        Supplier supplierDuck = new Supplier(1,"Duck Supplier", "best duck supplier");
+        Supplier supplierHorse = new Supplier(2, "Horse Supplier", "best horse supplier");
+        sl.add(supplierDuck);
+        sl.add(supplierHorse);
         testInstance.loadAll(sl);
         ArrayList<HashMap<String,String>> d = connInst.queryAllFromTable("supplier");
-        d.forEach((e) -> {
-            sl.forEach((s) -> {
-                if (s.getId() == Integer.parseInt(e.get("id"))) {
-                    assertEquals(s.getName(), e.get("name"));
-                    assertEquals(s.getDescription(), e.get("description"));
-                }
-            });
-        });
+        d.forEach((e) -> sl.forEach((s) -> {
+            if (s.getId() == Integer.parseInt(e.get("id"))) {
+                assertEquals(s.getName(), e.get("name"));
+                assertEquals(s.getDescription(), e.get("description"));
+            }
+        }));
     }
 }
