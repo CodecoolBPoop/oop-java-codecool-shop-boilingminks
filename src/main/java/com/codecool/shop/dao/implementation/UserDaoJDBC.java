7acab8041a2463265790186d20bd3e827a0e85ca
Akos Kolloros <kollorosa@gmail.com>
2019-02-13 13:30:10 +0100

    New User Dao

---
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoJDBC.java
@@ -0,0 +1,56 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.config.JdbcConnectivity;
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserDaoJDBC implements UserDao {
+    private static UserDaoJDBC instance = null;
+
+    private JdbcConnectivity JDBCInstance = JdbcConnectivity.getInstance();
+
+    public static UserDaoJDBC getInstance() {
+        if (instance == null) {
+            instance = new UserDaoJDBC();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(User user) {
+        JDBCInstance.executeQuery("INSERT INTO users (first_name, last_name, email, hashed_password, address, state, zip, country, is_admin)" +
+                "VALUES ('" + user.getFirstName() + "', '" + user.getLastName() + "', '" + user.getEmail() + "', '" + user.getHashedPassword() + "', '" + user.getAddress() + "','" + user.getState() + "', '" + user.getZip() + "', '" + user.getCountry() + "', '" + "false" + "');");
+    }
+
+    @Override
+    public User find(String email) {
+        List<HashMap<String, String>> hashMaps = JDBCInstance.executeQuerySelect("SELECT * FROM users WHERE email= '"+ email +"' ;");
+        List<User> users= getUserListFromHashMap(hashMaps);
+        return users.get(0);
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    private List<User> getUserListFromHashMap(List<HashMap<String, String>> hashMaps) {
+        List<User> resultList = new ArrayList<>();
+        for (HashMap<String, String> hashMap : hashMaps) {
+            User user = new User(hashMap.get("first_name"), hashMap.get("last_name"), hashMap.get("email"),
+                    hashMap.get("hashed_password"), hashMap.get("address"), hashMap.get("state"),
+                    hashMap.get("zip"), hashMap.get("country"));
+            resultList.add(user);
+        }
+        return resultList;
+    }
+}
