457f59106b6a9c4023c5c47e50a688ba6b7f6dec
zsombor <apathy.zsombor@gmail.com>
2019-02-14 14:13:10 +0100

    started userId

--- src/main/java/com/codecool/shop/controller/PaymentValidation.java
+++ src/main/java/com/codecool/shop/controller/PaymentValidation.java
@@ -1,16 +1,20 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.dao.implementation.OrderDaoJDBC;
 import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
+import com.codecool.shop.dao.implementation.UserDaoJDBC;
 import com.codecool.shop.model.Order;
 import com.codecool.shop.model.Transaction;
+import com.codecool.shop.model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -20,7 +24,11 @@ public class PaymentValidation extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         OrderDao orderDao = OrderDaoJDBC.getInstance();
-        int userId = 1; // TODO: USER SYSTEM!
+        UserDao userDao = UserDaoJDBC.getInstance();
+        HttpSession session = req.getSession(false);
+        User user = userDao.findByEmail((String) session.getAttribute("email"));
+        int userId = user.getId();
+        System.out.println(userId);
 
         HashMap<String, String> checkoutData = new HashMap<>();
         req.getParameterMap().entrySet().stream().forEach(e -> {
@@ -32,13 +40,12 @@ public class PaymentValidation extends HttpServlet {
         currentOrder.setTransaction(tran);
         currentOrder.saveToJson();
 
-        try{
+        try {
             String email = currentOrder.getUser().getEmail();
             String firstName = currentOrder.getUser().getFirstName();
 
             Emailer.mailTo(email, firstName);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
